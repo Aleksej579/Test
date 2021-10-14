186e221d26890c9b68004eaeeef035a9dbbc0b4f
aleksej eremenko <aleksej.eremenko579@gmail.com>
2021-10-15 00:12:16 +0300

    create last blok and start dialog

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import "./style.scss";
 // https://swiperjs.com/get-started
 var swiper = new Swiper(".mySwiper", { loop: true, });
 
+let dialog = document.querySelector('#dialog');
+let btn_dialog_hide = document.querySelector('#btn_dialog_hide');
+let signIn = document.querySelector('#signIn');
+
+signIn.onclick = function () {
+    dialog.style.display = 'block';
+}
+
+btn_dialog_hide.onclick = function () {
+    dialog.style.display = 'none';
+}
